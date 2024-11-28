78b42b5e20d74d5e4491337b822d934e0bcb5ef4
zayathena <claremaryregan@outlook.com>
2024-11-28 14:49:26 +0000

    connection file changed

--- db/connection.js
+++ db/connection.js
@@ -6,19 +6,18 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-if (ENV === "production") {
+if (process.env.DATABASE_URL) {
+ 
   config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
+  config.max = 2; 
+} else {
+
+  config.database = process.env.PGDATABASE;
 }
 
 module.exports = new Pool(config);
-
-module.exports = new Pool();
