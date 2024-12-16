94bb53b7ebbec95c459129b935464344e5d3a6d3
zayathena <claremaryregan@outlook.com>
2024-12-16 10:09:03 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const { getAPI, getTopics, getArticlesById, getArticles, getArticleComments, postArticleComment } = require("./app.controller");
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/api', getAPI);
 app.get('/api/topics', getTopics);
@@ -10,6 +12,7 @@ app.get('/api/articles/:article_id', getArticlesById);
 app.get('/api/articles', getArticles);
 app.get('/api/articles/:article_id/comments', getArticleComments)
 app.post('/api/articles/:article_id/comments', postArticleComment)
+// app.patch('/api/articles/:article_id', patchArticleVotes)
 
 app.use((err, req, res, next) => {
   if (err.status) {
